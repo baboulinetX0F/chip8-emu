6792fe3daed41ebdb0b088f033ca3673845f1a77
Unknown <vergilotaku@live.fr>
2017-01-22 01:33:59 +0100

    added all key input

--- Renderer.cpp
+++ Renderer.cpp
@@ -62,37 +62,106 @@ void Renderer::PollKeys(unsigned char* keys)
     switch( event.type ){
         case SDL_KEYDOWN:
             switch( event.key.keysym.sym ){
-                    case SDLK_UP:
+                    case SDLK_1:
+                        keys[0x1] = 1;                       
+                        break;
+                    case SDLK_2:
                         keys[0x2] = 1;                       
                         break;
-                    case SDLK_LEFT:
-                        keys[0x4] = 1;                       
+                    case SDLK_3:
+                        keys[0x3] = 1;                       
                         break;
-                    case SDLK_RIGHT:
+                     case SDLK_4:
+                        keys[0xC] = 1;                       
+                        break;
+                    case SDLK_a:
+                        keys[0x4] = 1;
+                        break;
+                    case SDLK_e:
                         keys[0x6] = 1;                       
                         break;
-                    case SDLK_DOWN:
+                    case SDLK_z:
                         keys[0x5] = 1;                       
-                        break;                   
-                    default:
-                        printf("Key Pressed : %d\n", event.key.keysym.sym);
                         break;
+                    case SDLK_r:
+                        keys[0xD] = 1;                       
+                        break;
+                    case SDLK_q:
+                        keys[0x7] = 1;                       
+                        break;
+                    case SDLK_s:
+                        keys[0x8] = 1;                       
+                        break;
+                    case SDLK_d:
+                        keys[0x9] = 1;                       
+                        break;
+                    case SDLK_f:
+                        keys[0xE] = 1;                       
+                        break; 
+                    case SDLK_w:
+                        keys[0xA] = 1;                       
+                        break;
+                    case SDLK_x:
+                        keys[0x0] = 1;                       
+                        break;
+                    case SDLK_c:
+                        keys[0xB] = 1;                       
+                        break;
+                    case SDLK_v:
+                        keys[0xC] = 1;                       
+                        break;       
             }            
         break;                    
         case SDL_KEYUP:
             switch( event.key.keysym.sym ){
-                    case SDLK_UP:
+                    case SDLK_1:
+                        keys[0x1] = 0;                       
+                        break;
+                    case SDLK_2:
                         keys[0x2] = 0;                       
                         break;
-                    case SDLK_LEFT:
+                    case SDLK_3:
+                        keys[0x3] = 0;                       
+                        break;
+                     case SDLK_4:
+                        keys[0xC] = 0;                       
+                        break;
+                    case SDLK_a:
                         keys[0x4] = 0;
                         break;
-                    case SDLK_RIGHT:
+                    case SDLK_e:
                         keys[0x6] = 0;                       
                         break;
-                    case SDLK_DOWN:
+                    case SDLK_z:
                         keys[0x5] = 0;                       
-                        break;                    
+                        break;
+                    case SDLK_r:
+                        keys[0xD] = 0;                       
+                        break;
+                    case SDLK_q:
+                        keys[0x7] = 0;                       
+                        break;
+                    case SDLK_s:
+                        keys[0x8] = 0;                       
+                        break;
+                    case SDLK_d:
+                        keys[0x9] = 0;                       
+                        break;
+                    case SDLK_f:
+                        keys[0xE] = 0;                       
+                        break; 
+                    case SDLK_w:
+                        keys[0xA] = 0;                       
+                        break;
+                    case SDLK_x:
+                        keys[0x0] = 0;                       
+                        break;
+                    case SDLK_c:
+                        keys[0xB] = 0;                       
+                        break;
+                    case SDLK_v:
+                        keys[0xC] = 0;                       
+                        break;       
             }
         break;
     }
